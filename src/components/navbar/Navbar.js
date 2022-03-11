6d3078d97c929950812a2d5b10c50b96e0d70d7b
m-eminozdogan <m.eminozdogan@hotmail.com>
2022-03-11 20:40:04 +0300

    navbar & home done

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,16 +1,23 @@
 import { ArrowDropDown, Notifications, Search } from "@material-ui/icons";
+import { useState } from "react";
 import "./navbar.scss";
 
 function Navbar() {
+  const [isScrolled, setIsScrolled] = useState(false);
+
+  window.onscroll = () => {
+    setIsScrolled(window.pageYOffset === 0 ? false : true);
+    return () => (window.onscroll = null);
+  };
+
   return (
-    <div className="navbar">
+    <div className={isScrolled ? "navbar scrolled" : "navbar"}>
       <div className="container">
         <div className="left">
           <img
             src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/2060px-Netflix_2015_logo.svg.png"
             alt=""
           />
-
           <span>Homepage</span>
           <span>Series</span>
           <span>Movies</span>
